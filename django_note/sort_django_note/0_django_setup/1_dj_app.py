e1faa388facc369cc9b14aad9bfc2c597cbcd771
mrmabdullah <mohammodraselmia128@gmail.com>
2025-01-03 21:17:21 +0600

    sort note django app done

--- django_note/sort_django_note/0_django_setup/1_dj_app.py
+++ django_note/sort_django_note/0_django_setup/1_dj_app.py
@@ -1,4 +1,3 @@
-# active env + go project folder
 # create a django app
 """ 
 django-admin startapp (app name)
@@ -18,25 +17,13 @@ INSTALLED_APPS = [
 
                 # return redirect('/result/'+passw)
                 # return redirect('account:recherge3', pk=profile.pk)
-# views.py>>>>>>>>> funtion
-""" 
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return HttpResponse('ami ak bisal boka vai')
-
-def home(request):
-    z = 30+34
-    return HttpResponse(z)
-"""
-
 
 # views.py>>>>>>>>> class
 """
+from django.shortcuts import render
 from django.views import View
-class jekononam(View):
+
+class Home(View):
     def get(self, request):
         return render(request,'home/index.html')
 """
@@ -50,31 +37,14 @@ class jekononam(View):
 from django.urls import path, include 
 urlpatterns = [
     ...
-    path('', include('app name.urls')),
-]
-"""
-
-# app/urls.py>>>>>>>>> function
-""" 
-from django.urls import path
-from . import views
-app_name = 'account'
-urlpatterns = [
-    path('', views.home, name='home'),
+    path('', include('account.urls')),
 ]
 """
 
 # app/urls.py>>>>>>>>> class
 """
-path('class/', views.jekononam.as_view(), name='home'),
-"""
-
-#  app url a
-""" 
-
-from django.conf.urls.static import static
-from django.conf import settings
-
-
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from account  import views
+urlpatterns = [
+    path('', views.Home.as_view(), name='home'),
+]
 """
