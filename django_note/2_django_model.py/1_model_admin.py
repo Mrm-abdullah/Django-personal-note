0335c1fd2822bac6485c024fbe86fadc2d52ef0f
mrmabdullah <mohammodraselmia128@gmail.com>
2025-01-04 20:57:17 +0600

    admin.py update

--- django_note/2_django_model.py/1_model_admin.py
+++ django_note/2_django_model.py/1_model_admin.py
@@ -9,3 +9,14 @@ class NameAdmin(admin.ModelAdmin):
 admin.site.register(Name, NameAdmin)
 @admin.register(Name, NameAdmin) """
 
+
+# update
+from django.contrib import admin
+from .models import Profile
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Profile._meta.get_fields()]
+    search_fields = ('user__username', 'full_name')  # প্রয়োজনে সার্চ অপশন
+    list_filter = ('country', 'date_joined')  # প্রয়োজনে ফিল্টার
+
+admin.site.register(Profile, ProfileAdmin)
